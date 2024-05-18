a204f7d22dc444f165959352ff6eaef09579ce43
arc_user <xarv68@gmail.com>
2024-05-18 09:16:55 +0530

    testing1.1

--- test.py
+++ test.py
@@ -1,7 +1,66 @@
 import pandas as pd
 from faker import Faker
 
-class Gen:
+class customer:
     
     def customerDetails(self):
         pass
+
+    def AddressDetails(self):
+        pass
+
+    def membershipDetails(self):
+        pass
+
+
+
+
+class product:
+
+    def productDetails(self):
+        pass
+
+    def productCostHistory(self):
+        pass
+
+    def productLocation(self):
+        pass
+
+
+class cart:
+
+    def cartDetails(self):
+        pass
+
+    def cartDiscount(self):
+        pass
+
+class shipping:
+
+    def shippingDetails(self):
+        pass
+
+class order:
+
+    def orderDetails(self):
+        pass
+
+    def returnOrderDetails(self):
+        pass
+
+    def exchangeOrderDetails(self):
+        pass
+
+
+class inventory:
+    
+    def inventoryDetails(self):
+        pass
+
+    def supplierDetails(self):
+        pass
+
+    def stockMovementDetails(self):
+        pass 
+    
+
